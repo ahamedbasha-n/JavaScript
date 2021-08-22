c04477c9c086e9031f5666552d5116317670d9a9
Ahamed Basha N <69695103+ahamedbasha-n@users.noreply.github.com>
2021-08-22 22:26:51 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,10 @@
+var a=20;
+var b=10;
+const pi=3.14
+console.log("Add:",a+b);
+console.log("Sub:",a-b);
+console.log("Mul:",a*b);
+console.log("Div:",a/b);
+console.log("Rem:",a%b);
+console.log(pi);
+console.log("Dhanus Prepared");
---
+++ hello.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+console.log("This is JavaScript Repo");
+// This is a single line comment
+/* This is a 
+multiline comment */
